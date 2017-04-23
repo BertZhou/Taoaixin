fb533c5b27643564b6f492a3121a56a435525aa7
zwf <602715041@qq.com>
2017-04-23 16:02:24 +0800

    zwf04.23

--- resources/views/user/create/index.blade.php
+++ resources/views/user/create/index.blade.php
@@ -167,8 +167,11 @@
                     </div>
                     <div class="rows_wrap clearfix">
                         <div class="rows_content">
+                            <div>
+                            <img id="ImgPr"/>
+                            </div>
                             <div class="input_text_wrap clearfix success">
-                                <input type="file"   style="width: 395px;">
+                                <input type="file"   style="width: 395px;" id="up">
                             </div>
                         </div>
                         <div class="rows_title">
@@ -383,6 +386,71 @@
 
         </div>
     </div>
+    <script>
+jQuery.fn.extend({
+    uploadPreview: function(opts) {
+        var _self = this,
+            _this = $(this);
+        opts = jQuery.extend({
+            Img: "ImgPr",
+            Width: 100,
+            Height: 100,
+            ImgType: ["gif", "jpeg", "jpg", "bmp", "png"],
+            Callback: function() {}
+        }, opts || {});
+        _self.getObjectURL = function(file) {
+            var url = null;
+            if (window.createObjectURL != undefined) {
+                url = window.createObjectURL(file)
+            } else if (window.URL != undefined) {
+                url = window.URL.createObjectURL(file)
+            } else if (window.webkitURL != undefined) {
+                url = window.webkitURL.createObjectURL(file)
+            }
+            return url
+        };
+        _this.change(function() {
+            if (this.value) {
+                if (!RegExp("\.(" + opts.ImgType.join("|") + ")$", "i").test(this.value.toLowerCase())) {
+                    alert("选择文件错误,图片类型必须是" + opts.ImgType.join("，") + "中的一种");
+                    this.value = "";
+                    return false
+                }
+                //高版本Jquey使用  if ($.support.leadingWhitespace)
+                if ($.support.leadingWhitespace) { //低版本jquery中使用$.browser.msie
+                    try {
+                        $("#" + opts.Img).attr('src', _self.getObjectURL(this.files[0]))
+                    } catch (e) {
+                        var src = "";
+                        var obj = $("#" + opts.Img);
+                        var div = obj.parent("div")[0];
+                        _self.select();
+                        if (top != self) {
+                            window.parent.document.body.focus()
+                        } else {
+                            _self.blur()
+                        }
+                        src = document.selection.createRange().text;
+                        document.selection.empty();
+                        obj.hide();
+                        obj.parent("div").css({
+                            'filter': 'progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)',
+                            'width': opts.Width + 'px',
+                            'height': opts.Height + 'px'
+                        });
+                        div.filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = src
+                    }
+                } else {
+                    $("#" + opts.Img).attr('src', _self.getObjectURL(this.files[0]))
+                }
+                opts.Callback()
+            }
+        })
+    }
+});
+$("#up").uploadPreview({
+    Img: "ImgPr"
+});</script>
 @endsection
 
 
